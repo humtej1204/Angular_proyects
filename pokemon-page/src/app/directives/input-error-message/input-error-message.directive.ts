0f1513a09ec431f66904d703bf8af6662f9fe20e
Humberto Tejada <humtej1204@gmail.com>
2024-01-14 19:04:55 -0500

    [Refactor]: Refactor directive inputErrorMessage to recieve string and FormControl and manage its errors

--- pokemon-page/src/app/directives/input-error-message/input-error-message.directive.ts
+++ pokemon-page/src/app/directives/input-error-message/input-error-message.directive.ts
@@ -1,20 +1,38 @@
 import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroupDirective } from '@angular/forms';
 import { InputErrors } from 'src/app/utils/input-error-messages';
 
 @Directive({
   selector: '[inputErrorMessage]'
 })
 export class InputErrorMessageDirective implements OnInit {
-  @Input('inputErrorMessage') formControl!: AbstractControl | null;
+  @Input('inputErrorMessage') controlName!: string | FormControl;
+  formControl!: AbstractControl | null;
 
   constructor(
     private elemRef: ElementRef,
-    private renderer: Renderer2
-  ) { }
+    private renderer: Renderer2,
+    private formGroupDirective: FormGroupDirective,
+  ) {}
 
   ngOnInit(): void {
+    this.getFormControlAssociated();
     this.updateErrorMessage();
+  }
+
+  private getFormControlAssociated() {
+    if (this.controlName instanceof AbstractControl) {
+      this.formControl = this.controlName;
+      return;
+    }
+
+    const formGroup = this.formGroupDirective.form;
+    if (!formGroup) return console.error(
+      'FormGroup not found for the control.');
+
+    this.formControl = formGroup.get(this.controlName);
+    if (!this.formControl) return console.error(
+      `FormControl with name '${this.controlName}' not found in the form.`);
   }
 
   private updateErrorMessage(): void {
